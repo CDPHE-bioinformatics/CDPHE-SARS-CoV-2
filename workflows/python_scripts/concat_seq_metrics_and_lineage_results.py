95bdc621479417201dbfd2cae69739ef6d7296dc
molly-hetheringtonrauth <molly.hetheringtonrauth@cdphe.co.us>
2023-05-17 22:31:36 +0000

    cleaning up workspace data

--- workflows/python_scripts/concat_seq_metrics_and_lineage_results.py
+++ workflows/python_scripts/concat_seq_metrics_and_lineage_results.py
@@ -16,10 +16,9 @@
 ## major update; refactoring to match universal naming conventions of the workbook generator;
 ## also reads in the workbook path to pull various values
 
-# update 2023-05-16
-## minor update; 
-## reads in terra data table instead of workbook
-## reads in assembly_software_file instead of assembler version
+# update 2023-05-04
+## add CDC lineage groupings from https://covid.cdc.gov/covid-data-tracker/#variant-proportions
+## new aggregated_lineage column will contain CDC lineage grouping
 
 import argparse
 import sys
@@ -31,11 +30,10 @@ import re
 def getOptions(args=sys.argv[1:]):
     parser = argparse.ArgumentParser(description="Parses command.")
     parser.add_argument('--sample_name_array')
-    # parser.add_argument('--workbook_path')
     parser.add_argument("--cov_out_files",  help= "txt file with list of bam file paths")
     parser.add_argument('--percent_cvg_files', help = 'txt file with list of percent cvg file paths')
-    # parser.add_argument('--assembler_version')
     parser.add_argument('--pangolin_lineage_csv', help = 'csv output from pangolin')
+    parser.add_argument('--cdc_lineage_groups_json', help = 'json file containing lineage groups for aggregating lineages')
     parser.add_argument('--nextclade_clades_csv', help = 'csv output from nextclade parser')
     parser.add_argument('--nextclade_variants_csv')
     parser.add_argument('--nextclade_version')
@@ -167,8 +165,8 @@ def get_df_spike_mutations(variants_csv):
 
     return df
 
-def concat_results(sample_name_list, terra_data_table_path, project_name, 
-                   assembly_software_file, pangolin_lineage_csv,
+def concat_results(sample_name_list, workbook_path, project_name, 
+                   assembly_software_file, pangolin_lineage_csv, cdc_lineage_groups_json,
                     nextclade_clades_csv, nextclade_version,
                    cov_out_df, percent_cvg_df, spike_variants_df):
 
@@ -180,11 +178,12 @@ def concat_results(sample_name_list, terra_data_table_path, project_name,
     def create_fasta_header(sample_name):
         return 'CO-CDPHE-%s' % sample_name
     
+
     # read in assembly software file
     assembly_software_df = pd.read_csv(assembly_software_file, sep = '\t')
     assembly_software_df = assembly_software_df.drop (columns = 'project_name')
 
-    
+
     # create dataframe and fill with constant strings
     df = pd.DataFrame()
     df['sample_name'] = sample_name_list
@@ -192,14 +191,10 @@ def concat_results(sample_name_list, terra_data_table_path, project_name,
     df['analysis_date'] = str(date.today())
     for col in assembly_software_df.columns:
         df[col] = assembly_software_df.loc(0, col)
-    # df['assembler_version'] = assembler_version
-    print(df)
+
 
     # read in workbook
-    workbook = pd.read_csv(terra_data_table_path, sep = '\t', dtype = {'sample_name' : object, 'hsn' : object})
-    # drop entity column
-    drop_col = workbook.columns.tolist()[0]
-    workbook = workbook.drop(columns = drop_col)
+    workbook = pd.read_csv(workbook_path, sep = '\t', dtype = {'sample_name' : object, 'hsn' : object})
     workbook = workbook.set_index('sample_name')
 
     # read in panlogin results
@@ -225,6 +220,10 @@ def concat_results(sample_name_list, terra_data_table_path, project_name,
     pangolin = pangolin.drop(columns = 'fasta_header')
     pangolin = pangolin.set_index('sample_name')
 
+    # read in list of CDC lineage groups
+    cdc_lineage_groups_df = pd.read_json(cdc_lineage_groups_json)
+    aggregated_lineage_df = aggregate_lineage(pangolin, cdc_lineage_groups_df)
+
 
     # read in nextclade csv
     nextclade = pd.read_csv(nextclade_clades_csv, dtype = {'sample_name' : object})
@@ -236,7 +235,7 @@ def concat_results(sample_name_list, terra_data_table_path, project_name,
     nextclade = nextclade.set_index('sample_name')
 
 
-    # set index on the samtools_df and percent_cvg_df and variants_df to prepare for joining
+    # set index on sample_names to prepare for joining
     cov_out_df = cov_out_df.set_index('sample_name')
     percent_cvg_df = percent_cvg_df.set_index('sample_name')
     spike_variants_df = spike_variants_df.set_index('sample_name')
@@ -248,6 +247,7 @@ def concat_results(sample_name_list, terra_data_table_path, project_name,
     j = j.join(cov_out_df, how = 'left')
     j = j.join(nextclade, how = 'left')
     j = j.join(pangolin, how = 'left')
+    j = j.join(aggregated_lineage_df, how = 'left')
     j = j.join(spike_variants_df, how = 'left')
     j = j.reset_index()
     # print(j)
@@ -269,8 +269,8 @@ def concat_results(sample_name_list, terra_data_table_path, project_name,
     columns.sort()
     primary_columns = ['hsn', 'sample_name', 'project_name', 'plate_name', 
                        'run_name', 'analysis_date', 'run_date', 'assembly_pass', 
-                 'percent_coverage', 'nextclade', 'pangolin_lineage', 
-                 'expanded_lineage', 'spike_mutations']
+                       'percent_coverage', 'nextclade', 'pangolin_lineage', 
+                       'expanded_lineage', 'aggregated_lineage', 'spike_mutations']
     for column in columns:
          if column not in primary_columns:
               primary_columns.append(column)
@@ -278,50 +278,41 @@ def concat_results(sample_name_list, terra_data_table_path, project_name,
     j = j[primary_columns]
 
 
-
-#     # add in 'failed assembly" in missing columns
-#     j.spike_mutations = j.spike_mutations.fillna(value = '')
-#     j.nextclade = j.nextclade.fillna(value = '')
-#     j.nextclade_version = j.nextclade_version.fillna(value = next_version)
-    # j.pangolin_lineage = j.pangolin_lineage.fillna(value = 'not assembled')
-    # j.expanded_lineage = j.expanded_lineage.fillna(value = 'not assembled')
-    # j.percent_non_ambigous_bases = j.percent_non_ambigous_bases.fillna(value = 0)
-#     j.mean_depth = j.mean_depth.fillna(value = 0)
-#     j.number_aligned_bases = j.number_aligned_bases.fillna(value = 0)
-#     j.number_seqs_in_fasta = j.number_seqs_in_fasta.fillna(value = 0)
-#     j.num_reads = j.num_reads.fillna(value = 0)
-#     j.mean_base_quality = j.mean_base_quality.fillna(value = 0)
-#     j.mean_map_quality = j.mean_map_quality.fillna(value = 0)
-#     j.number_N_bases = j.number_N_bases.fillna(value = 29903)
-#     j.pangolin_version = j.pangolin_version.fillna(value = pangolin_version )
-# #     j.pangolin_designation_version = j.pangolin_designation_version.fillna(value = pango_learn_version )
-#     j.assembler_version = j.assembler_version.fillna(value = assembler_version_txt)
-
-
-    # force integers
-    j.N_bases = j.N_bases.astype('Int64')
-    j.aligned_bases = j.aligned_bases.astype('Int64')
-    j.index_set = j.index_set.astype('Int64')
-    j.mapped_reads = j.mapped_reads.astype('Int64')
-    j.non_ambiguous_bases = j.non_ambiguous_bases.astype('Int64')
-    j.read_length = j.read_length.astype('Int64')
-    j.total_AA_deletions = j.total_AA_deletions.astype('Int64')
-    j.total_AA_substitutions = j.total_AA_substitutions.astype('Int64')
-    j.total_nucleotide_deletions = j.total_nucleotide_deletions.astype('Int64')
-    j.total_nucleotide_insertions = j.total_nucleotide_insertions.astype('Int64')
-    j.total_nucleotide_mutations = j.total_nucleotide_mutations.astype('Int64')
-
     outfile = '%s_sequencing_results.csv' % project_name
     j.to_csv(outfile, index = False)
 
     return j
 
-# def get_project_name(workbook_path):
-#     df = pd.read_csv(workbook_path, sep = '\t')
-#     project_name = df.project_name[0]
+def aggregate_lineage(pangolin_df, cdc_lineage_groups_df):
+    aggregated_lineage_df = pangolin_df[['expanded_lineage']].copy()
 
-#     return project_name
+    def get_cdc_grouping(expanded_lineage, cdc_lineage_groups_df):
+        potential_matches = []
+        for cdc_expanded_lineage in cdc_lineage_groups_df['expanded_lineage']:
+            if (expanded_lineage == cdc_expanded_lineage 
+                or expanded_lineage.startswith(cdc_expanded_lineage + '.')
+            ):
+                potential_matches.append(cdc_expanded_lineage)
 
+        # get most specific CDC grouping 
+        # example: if XBB.1.5.2, aggregate to XBB.1.5, not XBB
+        try:
+            match = max(potential_matches, key=len)
+            aggregated_lineage = cdc_lineage_groups_df.loc[cdc_lineage_groups_df['expanded_lineage'] == match, 'pangolin_lineage'].item()
+        except ValueError:
+            if expanded_lineage == 'Unassigned':
+                aggregated_lineage = 'Unassigned'
+            else:
+                aggregated_lineage = 'Other'
+        
+        return aggregated_lineage
+
+
+    aggregated_lineage_df['aggregated_lineage'] = aggregated_lineage_df.apply(lambda x: get_cdc_grouping(x['expanded_lineage'],
+                                                                                                         cdc_lineage_groups_df), axis=1)
+    aggregated_lineage_df.drop('expanded_lineage', axis=1, inplace=True)
+    
+    return aggregated_lineage_df
 
 def make_wgs_horizon_output (results_df, project_name):
 
@@ -330,14 +321,6 @@ def make_wgs_horizon_output (results_df, project_name):
 
     # rename columns 
     results_df = results_df.rename(columns = {'hsn' : 'accession_id', 'pango_designation_version' : 'pangoLEARN_version'})
-
-    # # add pangolin to the as prefix to version
-    # # pull out panglin version 
-    
-    # for row in range(results_df.shape[0]):
-    #     version = results_df.pangolin_version[row]
-    #     new_version_txt = f'pangolin {version}'
-    #     results_df.at[row, 'pangolin_version'] = new_version_txt
 
     col_order = ['accession_id', 'percent_coverage', 'pangolin_lineage', 'pangolin_version',
                  'report_to_epi', 'Run_Date', 'pangoLEARN_version']
@@ -353,14 +336,14 @@ if __name__ == '__main__':
     options = getOptions()
 
     sample_name_array = options.sample_name_array
-    terra_data_table_path = options.terra_data_table_path
+    workbook_path = options.workbook_path
     cov_out_files = options.cov_out_files
     percent_cvg_files = options.percent_cvg_files
-    assembly_software_file = options.assembly_software_file
+    assembler_version = options.assembler_version
     project_name = options.project_name
 
     pangolin_lineage_csv = options.pangolin_lineage_csv
-    # pangolin_version = options.pangolin_version
+    cdc_lineage_groups_json = options.cdc_lineage_groups_json
 
     nextclade_clades_csv = options.nextclade_clades_csv
     nextclade_variants_csv = options.nextclade_variants_csv
@@ -380,10 +363,11 @@ if __name__ == '__main__':
 
     # create results file
     results_df = concat_results(sample_name_list = sample_name_list,
-                                terra_data_table_path = terra_data_table_path,
+                                workbook_path = workbook_path,
                                 project_name = project_name,
-                                assembly_software_file= assembly_software_file,
+                                assembler_version = assembler_version,
                                 pangolin_lineage_csv=pangolin_lineage_csv,
+                                cdc_lineage_groups_json=cdc_lineage_groups_json,
                                 nextclade_clades_csv=nextclade_clades_csv,
                                 nextclade_version=nextclade_version,
                                 cov_out_df=cov_out_df,
@@ -396,153 +380,3 @@ if __name__ == '__main__':
     
 
     print('DONE!')
-
-
-#def get_df_spike_mutations(variants_csv):
-
-#     def get_accession_id(fasta_header):
-#         accession_id = str(re.findall('CO-CDPHE-([0-9a-zA-Z_\-\.]+)', fasta_header)[0])
-#         return accession_id
-
-#     variants = pd.read_csv(variants_csv, dtype = {'accession_id' : object})
-#     if variants.shape[0] == 0 :
-#         print('there are no accession_ids in nextclade variants file; likely all your sequences were bad')
-#     variants = variants.rename(columns = {'accession_id' : 'fasta_header'})
-
-#     accession_id = variants.apply(lambda x:get_accession_id(x.fasta_header), axis = 1)
-#     variants.insert(value = accession_id, loc = 0, column = 'accession_id')
-#     variants = variants.drop(columns = 'fasta_header')
-
-#     #### filter variants for spike protein varaints in rbd and pbcs #####
-
-#     crit = variants.gene == 'S'
-#     critRBD = (variants.codon_position >= 461) & (variants.codon_position <= 509)
-#     critPBCS = (variants.codon_position >= 677) & (variants.codon_position <= 694)
-#     crit732 = variants.codon_position == 732
-#     crit452 = variants.codon_position == 452
-#     crit253 = variants.codon_position == 253
-#     crit13 = variants.codon_position == 13
-#     crit145 = variants.codon_position == 145 # delta plus AY.4.2
-#     crit222 = variants.codon_position == 222 # delta plus AY.4.2
-#     critdel = variants.variant_name.str.contains('del')
-
-#     variants_general = variants[crit & (critRBD | critPBCS | crit732 | critdel | crit452 | crit253 | crit13 | crit145 | crit222)]
-
-#     ##### special filter for omicron variants ######
-#     omicron_spike_mutations_list = ['S_G339D', 'S_S371L', 'S_S373LP', 'S_S375F', 'S_N440K', 'S_G446S', 'S_E484K', 'S_Q493K',
-#                                    'S_Q486S', 'S_Q498R', 'S_Y505H', 'S_T547K', 'S_N764K', 'S_N856K', 'S_Q954H', 'S_N969K', 'S_L981F',
-#                                    '_ins22205GAGCCAGAA', 'S_G142del', 'S_V143del', 'S_Y144del', 'S_N211del']
-
-#     crit_omicron = variants.variant_name.isin(omicron_spike_mutations_list)
-#     crit_omicron_insertion_1 = variants.variant_name.str.contains('ins')
-#     crit_omicron_insertion_2 = variants.codon_position == 22205
-
-#     omicron_variants = variants[crit_omicron | (crit_omicron_insertion_1 & crit_omicron_insertion_2) ]
-
-
-#     #### special filter for delta + (AY.4.2) mutations #####
-#     delta_plus_spike_mutations_list = ['S_Y145H', 'S_A222V']
-#     crit_delta_plus = variants.variant_name.isin(delta_plus_spike_mutations_list)
-#     delta_plus_variants = variants[crit_delta_plus]
-
-
-#     ####### get list of spike mutations in the poly cleavage site and the rbd
-#     ## use variants_general
-#     accession_ids = variants_general.accession_id.unique().tolist()
-
-#     df = pd.DataFrame()
-#     accession_id_list = []
-#     variant_name_list = []
-
-#     seperator = '; '
-
-#     for accession_id in accession_ids:
-#         accession_id_list.append(accession_id)
-
-#         crit = variants_general.accession_id == accession_id
-#         f = variants_general[crit]
-#         f = f.reset_index()
-
-#         mutations = []
-#         for row in range(f.shape[0]):
-#             mutations.append(f.variant_name[row])
-#         mutations_string = seperator.join(mutations)
-#         variant_name_list.append(mutations_string)
-
-#     df['accession_id'] = accession_id_list
-#     df['spike_mutations'] = variant_name_list
-
-
-#     #### make special column for omicron varints ######
-#     ## use omicron_variants
-
-#     if omicron_variants.shape[0] != 0:
-#         accession_ids = omicron_variants.accession_id.unique().tolist()
-
-#         df_omicron = pd.DataFrame()
-#         accession_id_list = []
-#         omicron_variant_list = []
-
-#         seperator = '; '
-
-#         for accession_id in accession_ids:
-#             accession_id_list.append(accession_id)
-
-#             crit = omicron_variants.accession_id == accession_id
-#             f = omicron_variants[crit]
-#             f = f.reset_index()
-
-#             mutations = []
-#             for row in range(f.shape[0]):
-#                 mutations.append(f.variant_name[row])
-#             mutations_string = seperator.join(mutations)
-#             omicron_variant_list.append(mutations_string)
-
-#         df_omicron['accession_id'] = accession_id_list
-#         df_omicron['omicron_spike_mutations'] = omicron_variant_list
-
-#         df_omicron = df_omicron.set_index('accession_id')
-#         df = df.set_index('accession_id')
-#         df = df.join(df_omicron, how = 'left')
-#         df = df.reset_index()
-
-#     else:
-#         df['omicron_spike_mutations'] = ''
-
-#     #### make special column for delta plus varints ######
-#     ### use delta_plus_variants
-#     if delta_plus_variants.shape[0] != 0:
-#         accession_ids = delta_plus_variants.accession_id.unique().tolist()
-
-#         df_delta_plus = pd.DataFrame()
-#         accession_id_list = []
-#         delta_plus_variants_list = []
-
-#         seperator = '; '
-
-#         for accession_id in accession_ids:
-#             accession_id_list.append(accession_id)
-
-#             crit = delta_plus_variants.accession_id == accession_id
-#             f = delta_plus_variants[crit]
-#             f = f.reset_index()
-
-#             mutations = []
-#             for row in range(f.shape[0]):
-#                 mutations.append(f.variant_name[row])
-#             mutations_string = seperator.join(mutations)
-#             delta_plus_variants_list.append(mutations_string)
-
-#         df_delta_plus['accession_id'] = accession_id_list
-#         df_delta_plus['delta_plus_spike_mutations'] = delta_plus_variants_list
-
-#         df_delta_plus = df_delta_plus.set_index('accession_id')
-#         df = df.set_index('accession_id')
-#         df = df.join(df_delta_plus, how = 'left')
-#         df = df.reset_index()
-
-#     else:
-#         df['delta_plus_spike_mutations'] = ''
-
-
-#     return df
