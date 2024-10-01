e08e7845f755fa5c52fe2caedc8b4cd1798de59b
arianna-smith <arianna.smith@state.co.us>
2024-10-01 17:40:47 -0600

    add try except for bug when there are no variants

--- scripts/nextclade_json_parser.py
+++ scripts/nextclade_json_parser.py
@@ -195,7 +195,10 @@ def get_nextclade(json_path, project_name, workflow_version):
 
     df['fasta_header'] = sample_name_list
     df['sample_name'] = df.apply(lambda x:get_sample_name_from_fasta_header(x.fasta_header), axis = 1)
-    df['hsn'] = df.apply(lambda x:hsn_from_id(x.sample_name), axis = 1)
+    try:
+        df['hsn'] = df.apply(lambda x:hsn_from_id(x.sample_name), axis = 1)
+    except ValueError:
+        df['hsn'] = None
     df['nextclade'] = clade_list
     df['total_nucleotide_mutations'] = totalSubstitutions_list
     df['total_nucleotide_deletions'] = totalDeletions_list
